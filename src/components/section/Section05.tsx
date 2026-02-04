b7381bf3d128d4a5a0379f24334ad9461b86d4a0
etranger2268 <shun.5.2268@gmail.com>
2026-02-04 19:37:46 +0900

    develop-06-34

--- src/components/section/Section05.tsx
+++ src/components/section/Section05.tsx
@@ -5,16 +5,17 @@ import UserCard from '@/components/organism/user/UserCard';
 import DefaultLayout from '@/components/template/DefaultLayout';
 import HeaderOnlyLayout from '@/components/template/HeaderOnlyLayout';
 
-const user = {
+const users = [...Array(10).keys()].map((key) => ({
+  key,
   image: '/victor-g-N04FIfHhv_k-unsplash.jpg',
-  name: 'etranger2268',
+  name: `etranger2268-${key}`,
   email: '123@gmail.com',
   tel: '0120-0000-000',
   company: {
     name: '123株式会社',
   },
   website: '123.com',
-};
+}));
 
 const Section05 = () => {
   const [path, setPath] = useState<'TOP' | 'USERS'>('TOP');
@@ -28,7 +29,10 @@ const Section05 = () => {
   if (path === 'TOP') {
     return (
       <DefaultLayout onClick={handleSetPath}>
-        <Section05Content />
+        <div className="mb-2">
+          <h1 className="text-xl font-bold">TOPページ</h1>
+        </div>
+        {/* <Section05Content /> */}
       </DefaultLayout>
     );
   }
@@ -36,6 +40,9 @@ const Section05 = () => {
   if (path === 'USERS') {
     return (
       <HeaderOnlyLayout onClick={handleSetPath}>
+        <div className="mb-2">
+          <h1 className="text-xl font-bold">ユーザー一覧</h1>
+        </div>
         <Section05Content />
       </HeaderOnlyLayout>
     );
@@ -61,7 +68,11 @@ function Section05Content() {
       </div>
       <div className="border rounded p-2">
         <h2 className="text-lg font-bold text-gray-800">organism</h2>
-        <UserCard user={user} />
+        <div className="grid grid-cols-2 gap-4">
+          {users.map((user) => (
+            <UserCard key={user.key} user={user} />
+          ))}
+        </div>
       </div>
     </main>
   );
