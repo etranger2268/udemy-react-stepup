ada17c78450950a2b61f6c2891b21c0fde76059d
etranger2268 <shun.5.2268@gmail.com>
2026-02-09 22:25:32 +0900

    develop-10-56

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,13 +1,14 @@
+import type { JSX } from 'react';
 import type { TodoType } from '@/types/todo';
 
 // type TodoPickType = Pick<TodoType, 'userId' | 'title' | 'completed'>;
 // type TodoOmitType = Omit<TodoType, 'id'>;
 
-const Todo = ({ todo }: { todo: TodoType }) => {
+const Todo = ({ todo }: { todo: TodoType }): JSX.Element => {
   const mark = todo.completed ? '✓' : '☐';
   const index = String(todo.id).padStart(3, '0');
   return (
-    <li key={todo.id} className="border-b border-gray-300 py-1 px-2">
+    <li key={todo.id} className="border-b border-gray-300 py-1 px-2 text-sm">
       {index}. {mark} {todo.title} - userId: {todo.userId}
     </li>
   );
