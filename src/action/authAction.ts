e07238cd796d33000ce9471ad52d56baa3749ca0
etranger2268 <shun.5.2268@gmail.com>
2026-02-11 11:32:27 +0900

    develop-12-75

---
+++ src/action/authAction.ts
@@ -0,0 +1,32 @@
+'use server';
+
+type State = {
+  error?: string;
+  success: boolean;
+};
+
+export const authAction = async (_prevState: State, formData: FormData) => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const userId = formData.get('userId') as string;
+
+    if (!userId) {
+      return { error: 'ユーザーIDを入力してください', success: false };
+    }
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+    if (!res.ok) {
+      return { error: 'ユーザーが見つかりませんでした', success: false };
+    }
+
+    return { success: true };
+  } catch (err: unknown) {
+    console.error(err);
+
+    return {
+      error: err instanceof Error ? err.message : '予期せぬエラーが発生しました',
+      success: false,
+    };
+  }
+};
